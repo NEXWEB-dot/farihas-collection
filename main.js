6b5b29925177648bcdac1555ca7fa6d85594c6c8
Shayan Faisal <shayanaptech1234@gmail.com>
2026-07-04 21:30:04 +0500

    fist commit

--- main.js
+++ main.js
@@ -292,6 +292,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 <a href="index.html" class="menu-link"><i class="ph ph-house"></i> Home</a>
                 <a href="shoes (1).html" class="menu-link"><i class="ph ph-sneaker"></i> All Shoes</a>
                 <a href="reviews.html" class="menu-link"><i class="ph ph-star"></i> Reviews</a>
+                <a href="shipping-policy.html" class="menu-link"><i class="ph ph-truck"></i> Shipping Policy</a>
                 <a href="about-us.html" class="menu-link"><i class="ph ph-info"></i> About Us</a>
                 <a href="privacy-policy.html" class="menu-link"><i class="ph ph-shield-check"></i> Privacy Policy</a>
                 <a href="terms-conditions.html" class="menu-link"><i class="ph ph-file-text"></i> Terms & Conditions</a>
